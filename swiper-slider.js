8df8d6b4b234dca66bf87a97489a82d5549cd4c5
likersX214 <131512886+likersX214@users.noreply.github.com>
2023-10-20 14:46:37 +0300

    Add files via upload

---
+++ swiper-slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
